a304233214f05404fbf750a22e829148eb2431a5
francobustos23 <bustosfranco071@gmail.com>
2024-05-13 21:39:55 -0300

    changes for validations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PreloadedData } from './prisma/data/preloaded';
 import { PrismaService } from './prisma/prisma.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [],
   providers: [PreloadedData, PrismaService],
 })
