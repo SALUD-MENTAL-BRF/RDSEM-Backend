7368ef21400afc55761e7165f6435672e125b479
brian Céspedes aguilera <cespedes.aguilera.brian@gmail.com>
2024-05-15 20:05:05 -0300

    add config of the cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PreloadedData } from './prisma/data/preloaded';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:4000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
+
   await app.listen(3000);
 
   
