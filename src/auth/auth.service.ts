bddf975cf67ae3157dabdefc73589b702f2bdb6d
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-05-28 10:23:49 -0300

    added logic for login with google account

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,13 +5,19 @@ import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 import * as bcryptjs from 'bcryptjs';
 import { jwtConstants } from './constants/jwt.constant';
+import { OAuth2Client } from 'google-auth-library';
 
 @Injectable()
 export class AuthService {
+
+  private client: OAuth2Client;
+
   constructor(
     private readonly usersService: UsersService,
     private readonly jwtService: JwtService
-  ) {}
+  ) {
+    this.client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+  }
 
   async register({ password, email, username }: RegisterDto) {
     let user = await this.usersService.findOneByEmail(email);
@@ -34,7 +40,7 @@ export class AuthService {
       password: hashedPassword,
     });
   
-    const payload = { email: user.email, password: hashedPassword };
+    const payload = { email: user.email, sub: user.id };
   
     const token = await this.jwtService.signAsync(payload, {
       secret: jwtConstants.secret
@@ -61,7 +67,7 @@ export class AuthService {
       throw new Error('Invalid credentials');
     }
 
-    const payload = { email: user.email };
+    const payload = { email: user.email, sub: user.id };
 
     const token = await this.jwtService.signAsync(payload, {
       secret: jwtConstants.secret
@@ -74,4 +80,43 @@ export class AuthService {
 
     return data
   }
+
+  async googleLogin(tokenId: string){
+    const ticket = await this.client.verifyIdToken({
+      idToken: tokenId,
+      audience: process.env.GOOGLE_CLIENT_ID
+    });
+
+    const payload = ticket.getPayload()
+  
+    if (!payload) {
+      throw new BadRequestException('Error en la verificación del token de google')
+    }
+
+    let user = await this.usersService.findOneByEmail(payload.email)
+
+    if (!user) {
+      user = await this.usersService.CreateUser({
+        username: payload.name,
+        email: payload.email,
+        googleId: payload.sub,
+        imageUrl: payload.picture
+      })
+
+      const jwtPayload = { email: user.email, sub: user.id }
+
+      const token = await this.jwtService.signAsync(jwtPayload, {
+        secret: jwtConstants.secret
+      })
+
+      const data = {
+        token,
+        user
+      }
+
+      return data
+    }
+
+  }
+
 }
