d2cf7b21706da40cced37b48139e14e705438a40
brian Céspedes aguilera <cespedes.aguilera.brian@gmail.com>
2024-07-29 18:02:37 -0300

    .

--- src/notes/notes.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { CreateNoteDto } from './dto/create-note.dto';
-import { UpdateNoteDto } from './dto/update-note.dto'
-
-@Injectable()
-export class NotesService {}
