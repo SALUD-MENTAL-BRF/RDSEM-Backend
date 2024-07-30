6b5ac393a704155e20b34d8fba2170d49061e4eb
brian Céspedes aguilera <cespedes.aguilera.brian@gmail.com>
2024-07-30 19:12:00 -0300

    Added endpoints to create and get notes by userId

--- src/note/note.controller.ts
+++ src/note/note.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { NoteService } from './note.service';
+import { CreateNoteDto } from './dto/createNoteDto';
 
 @Controller('note')
-export class NoteController {}
+export class NoteController {
+
+  constructor(private readonly notesService: NoteService) {} 
+
+  @Get('/:userId')
+  getNotesByUserId(@Param('userId') userId: string) {
+    const userIdNumber = parseInt(userId, 10);
+    if (isNaN(userIdNumber)) {
+      throw new BadRequestException('Invalid user ID');
+    }
+    return this.notesService.getNotesByUserId(userIdNumber);
+  }
+  
+
+  @Post('/')
+  createNote(@Body() createNoteDto: CreateNoteDto) {
+    return this.notesService.createNote(createNoteDto)
+  }
+
+
+}
