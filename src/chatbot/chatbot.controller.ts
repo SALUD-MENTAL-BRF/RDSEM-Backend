d7a2d6082c773e1e11fe29fd6a444286372bbc0e
Rodriasdev <veronrodrigo98@gmail.com>
2024-07-30 20:39:45 -0300

    the openai library was installed

--- src/chatbot/chatbot.controller.ts
+++ src/chatbot/chatbot.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post,Body } from "@nestjs/common";
+import { ChatbotService } from "./chatbot.service";
 
 @Controller('chat')
-
 export class ChatBotController{
-    
+    constructor(private chatBotService: ChatbotService){}
+
+    @Post('/')
+    async QuestionChat(@Body() msg: any){
+        return this.chatBotService.askAI(msg)
+    }
 }
--- src/chatbot/chatbot.service.ts
+++ src/chatbot/chatbot.service.ts
@@ -3,4 +3,7 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class ChatbotService{
     
+    async askAI(msg:any){
+        return msg
+    }
 }
