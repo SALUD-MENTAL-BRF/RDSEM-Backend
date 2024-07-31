f3070da3f168349fed8a84c19422f024d6952c71
brian Céspedes aguilera <cespedes.aguilera.brian@gmail.com>
2024-07-31 14:09:44 -0300

    The route to get the notes by user ID was fixed

--- src/note/note.controller.ts
+++ src/note/note.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { NoteService } from './note.service';
 import { CreateNoteDto } from './dto/createNoteDto';
 
@@ -7,8 +7,8 @@ export class NoteController {
 
   constructor(private readonly notesService: NoteService) {} 
 
-  @Get('/:userId')
-  getNotesByUserId(@Param('userId') userId: string) {
+  @Get()
+  getNotesByUserId(@Query('userId') userId: string) {
     const userIdNumber = parseInt(userId, 10);
     if (isNaN(userIdNumber)) {
       throw new BadRequestException('Invalid user ID');
