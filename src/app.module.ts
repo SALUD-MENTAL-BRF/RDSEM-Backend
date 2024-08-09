6b26f590095dde887c87043714856180ffedd9ee
Rodriasd <veronrodrigo98@gmail.com>
2024-08-09 09:31:33 -0300

    was added the controller and service of the request of the professionals

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { NoteModule } from './note/note.module';
+import { Req_ProfessionalModule } from './request_professional/req_professional.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, NoteModule],
+  imports: [UsersModule, AuthModule, NoteModule,Req_ProfessionalModule],
   controllers: [],
   providers: [PreloadedData, PrismaService],
 })
--- src/request_professional/req_professional.module.ts
+++ src/request_professional/req_professional.module.ts
@@ -1,4 +1,8 @@
 import { Module } from "@nestjs/common";
-
-@Module({})
+import { Req_ProfessionalController } from "./req_professional.controller";
+import { Req_ProfessionalService } from "./req_professional.service";
+@Module({
+    controllers:[Req_ProfessionalController],
+    providers: [Req_ProfessionalService]
+})
 export class Req_ProfessionalModule{}
