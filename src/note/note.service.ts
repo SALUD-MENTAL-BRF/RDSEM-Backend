f7b73cb2918bd82d719b5e153b12093a4a09d62e
brian Céspedes aguilera <cespedes.aguilera.brian@gmail.com>
2024-08-11 14:30:54 -0300

    rearranged code

--- src/note/note.service.ts
+++ src/note/note.service.ts
@@ -7,12 +7,14 @@ export class NoteService {
 
   constructor(private readonly prisma: PrismaService) {}
 
+  createNote(data: CreateNoteDto) {
+    return this.prisma.note.create({data})
+  }
+
   getNotesByUserId(userId: number) {
     return this.prisma.note.findMany({ where: { userId } })
   }
 
-  createNote(data: CreateNoteDto) {
-    return this.prisma.note.create({data})
-  }
+  
 
 }
