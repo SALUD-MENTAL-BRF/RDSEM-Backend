98ac8de52c52bb988e9995c63d41533a5d62a734
Rodriasd <veronrodrigo98@gmail.com>
2024-08-15 11:53:24 -0300

    was deleted unnecessary files

--- src/request_professional/req_professional.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { Req_ProfessionalController } from "./req_professional.controller";
-import { Req_ProfessionalService } from "./req_professional.service";
-@Module({
-    controllers:[Req_ProfessionalController],
-    providers: [Req_ProfessionalService]
-})
-export class Req_ProfessionalModule{}
