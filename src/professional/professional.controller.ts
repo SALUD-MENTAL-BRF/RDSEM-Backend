fe089fa3cc1207c0d4ade5141c38eb4b85d1dfba
Rodriasd <veronrodrigo98@gmail.com>
2024-08-21 12:12:38 -0300

    was added the service for create the profile of the professional

--- src/professional/professional.controller.ts
+++ src/professional/professional.controller.ts
@@ -11,11 +11,13 @@ export class ProfessionalControllers {
 
     @Post(':id')
     @UsePipes(new ValidationPipe({whitelist: true}))
-    async createProfessional(@Param('id') id: string ,@Req() _request: Request, @Res() response : Response, @Body() data: CreateProfessionalDto) {
+    async createProfessional(@Param('id') id: string ,@Req() _request: Request, @Res() response : Response, @Body() data: any) {
         try {
-            response.status(200).json(this.professionalService.AddProfessional(data, id))
+            const professional = await this.professionalService.AddProfessional(data, id)
+
+            response.status(200).json(professional)
         } catch (error) {
-            response.status(200).json({msg: 'Error to add the professional'})
+            response.status(500).json({msg: 'Error to add the professional'})
             console.log(error)
         }
     }
--- src/professional/professional.service.ts
+++ src/professional/professional.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Post, Body } from "@nestjs/common";
+import { Injectable, Post, Body, NotFoundException } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 
 
@@ -9,10 +9,52 @@ export class ProfessionalService {
     constructor(private readonly prismaService : PrismaService){};
 
     async AddProfessional(data:any, id: string){
-    
-        return this.prismaService.professional.create({data: data})
+        
+        const userId = Number(id)
+
+        const user = await this.prismaService.user.findFirst({where: {
+            id: userId
+        }})
+
+        if(!user){
+            throw new NotFoundException("No se encontro el usuario")
+        }
+
+        if(user.roleId == 2) {
+            return {msg: "El usuario ya esta registrado como un profesional."}
+        }
+        
+        await this.prismaService.user.update({
+            where: {id: userId},
+            data: {
+                roleId: 2
+            }
+        })
+
+
+        const profesional = await this.prismaService.professional.create({data: {...data, userId: userId}})
+
+        await this.createProfile(profesional.id)
+        
+        return profesional
+
+        
     };
 
 
+    async createProfile(id: number) {
+        return await this.prismaService.profileProfessional.create({
+          data: {
+            description: '', 
+            availability: false, 
+            preference_communication: '',
+            url_image: '', 
+            professional: {
+              connect: { id: id }
+            }
+          }
+        });
+      }
+      
 
 }
