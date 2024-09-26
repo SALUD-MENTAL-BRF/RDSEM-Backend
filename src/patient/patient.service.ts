ecb8a1d54e28c27d4fa8bad3a73f63d12c22d36a
Rodriasd <veronrodrigo98@gmail.com>
2024-09-26 09:48:41 -0300

    added service for find all the patients for professional

---
+++ src/patient/patient.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+
+
+@Injectable()
+export class PatientService {
+
+    constructor(private readonly prisma: PrismaService){}
+
+
+    async getAllPatientByProfessional(profesionalId:number){
+        return await this.prisma.patient.findMany({
+            include:{
+                professional:{
+                    where:{
+                        id: profesionalId
+                    }
+                }
+            }
+        });
+    };
+
+
+};
