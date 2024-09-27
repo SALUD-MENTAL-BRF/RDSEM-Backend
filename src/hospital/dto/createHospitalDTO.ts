f1964f9dcaa165d2919434a782d20aceb2c6011f
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-09-27 16:42:05 -0300

    dto createHospitalDTO

---
+++ src/hospital/dto/createHospitalDTO.ts
@@ -0,0 +1,17 @@
+enum TypeHospital {
+  Publico = "Publico",
+  Privado = "Privado"
+}
+
+export interface createHospital {
+  name: string;
+  address: string;
+  telephone: string;
+  email: string;
+  website?: string;
+  director?: string;
+  openingHours: string;
+  type: TypeHospital
+  specialties?: number[] | undefined;  // IDs de las especialidades
+  services?: number[] | undefined;     // IDs de los servicios
+}
