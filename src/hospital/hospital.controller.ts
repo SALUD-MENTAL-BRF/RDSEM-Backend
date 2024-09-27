acb49673c0e88c340e758288ee2be4a5b1706bb9
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-09-27 16:41:47 -0300

    logic to create and obtain hospitals

--- src/hospital/hospital.controller.ts
+++ src/hospital/hospital.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { HospitalService } from './hospital.service';
+import { createHospital } from './dto/createHospitalDTO';
 
 @Controller('hospital')
-export class HospitalController {}
+export class HospitalController {
+  constructor(private readonly hospitalService: HospitalService) {}
+
+  @Post('/')
+  async createHospital(@Body() hospital: createHospital) {
+    try {
+      await this.hospitalService.createHospitalService(hospital);
+      return { success: true };
+    } catch (error) {
+      return { error: 'No se pudo crear el hospital', message: error.message };
+    }
+  }
+
+  @Get('/')
+  async getAllHospitals() {
+    try {
+      const hospitals = await this.hospitalService.getAllHospitals();
+      return { hospitals };
+    } catch (err) {
+      if(err instanceof Error) {
+        return { success: false, message: err.message };
+      } else {
+        return { success: false, message: 'Error inesperado' };
+      }
+    }
+  }
+}
--- src/hospital/hospital.service.ts
+++ src/hospital/hospital.service.ts
@@ -1,4 +1,82 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { createHospital } from './dto/createHospitalDTO';
 
 @Injectable()
-export class HospitalService {}
+export class HospitalService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createHospitalService(hospital: createHospital) {
+    try {
+      const data: any = {
+        name: hospital.name,
+        address: hospital.address,
+        telephone: hospital.telephone,
+        email: hospital.email,
+        website: hospital.website,
+        director: hospital.director,
+        openingHours: hospital.openingHours,
+        type: hospital.type
+      };
+
+      // Si specialties existe y es un array no vacío, agrega las relaciones
+      if (Array.isArray(hospital.specialties) && hospital.specialties.length > 0) {
+        data.specialties = {
+          create: hospital.specialties.map(specialtyId => ({
+            specialty: {
+              connect: { id: specialtyId },
+            },
+          })),
+        };
+      }
+
+      // Si services existe y es un array no vacío, agrega las relaciones
+      if (Array.isArray(hospital.services) && hospital.services.length > 0) {
+        data.services = {
+          create: hospital.services.map(serviceId => ({
+            service: {
+              connect: { id: serviceId },
+            },
+          })),
+        };
+      }
+
+      const newHospital = await this.prismaService.hospital.create({
+        data,
+        include: {
+          specialties: true,
+          services: true,
+        },
+      });
+
+      return newHospital;
+    } catch (error) {
+      throw new Error(`Error al crear el hospital: ${error.message}`);
+    }
+  }
+
+  async getAllHospitals(){
+    return await this.prismaService.hospital.findMany({
+      include: {
+        specialties: {
+          include: {
+            specialty: {
+              select: {
+                name: true
+              }
+            }
+          }
+        },
+        services: {
+          include: {
+            service: {
+              select: {
+                name:true
+              }
+            }
+          }
+        }
+      }
+    });
+  }
+}
