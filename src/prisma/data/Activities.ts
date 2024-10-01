6c2627c1e14e9b081b191a01dcc708a59bd52a4f
Rodriasd <veronrodrigo98@gmail.com>
2024-10-01 10:14:52 -0300

    was added data of the activities

---
+++ src/prisma/data/Activities.ts
@@ -0,0 +1,26 @@
+export const Activities = [
+    {
+        "title": "Juegos de Memoria y Atención",
+        "categoryNaurodevelopmentId": 1
+    },
+    {
+        "title": "Juegos de Reconocimiento Emocional",
+        "categoryNaurodevelopmentId": 2
+    },
+    {
+        "title": "Juegos de Habilidades Sociales",
+        "categoryNaurodevelopmentId": 3,
+    },
+    {
+        "title": "Juegos de Coordinación Motora y Sensorial",
+        "categoryNaurodevelopmentId": 4
+    },
+    {
+        "title": "Juegos de Resolución de Problemas y Lógica",
+        "categoryNaurodevelopmentId": 5
+    },
+    {
+        "title": "Juegos de Relajación y Mindfulness",
+        "categoryNaurodevelopmentId": 6
+    }
+]
