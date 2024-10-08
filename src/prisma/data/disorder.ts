621d59518b2a7397f6c5b25727b42d065e544f9b
Rodriasd <veronrodrigo98@gmail.com>
2024-10-08 09:49:53 -0300

    was added the data of the disorder

---
+++ src/prisma/data/disorder.ts
@@ -0,0 +1,14 @@
+const disorder = [
+    {
+        "id": 1,
+        "type": "Ansiedad"
+    },
+    {
+        "id": 2,
+        "type": "Depresión"
+    },
+    {
+        "id":2,
+        "type": "Neurodesarrollo"
+    }
+]
