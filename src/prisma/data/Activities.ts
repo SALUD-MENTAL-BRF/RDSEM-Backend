d5e5403eefb575fbcc159491fab574c74b27e118
Rodriasd <veronrodrigo98@gmail.com>
2024-10-18 16:40:58 -0300

    was modified the schemas

--- src/prisma/data/Activities.ts
+++ src/prisma/data/Activities.ts
@@ -2,36 +2,42 @@ export const naurodevelopmentActivities = [
     {
         "title": "Memoria y Atención",
         "description": "El usuario debe recordar y repetir secuencias de sonidos o colores, ayudando a desarrollar la memoria y la atención selectiva.",
+        "active": false,
         "categoryActivitiesId": 1,
         "disorderId": 3,
     },
     {
         "title": "Reconocimiento Emocional",
         "description": "El usuario debe identificar emociones en caras mostradas, lo que ayuda a mejorar el reconocimiento y la comprensión emocional.",
+        "active": true,
         "categoryActivitiesId": 2,
         "disorderId": 3
     },
     {
         "title": "Habilidades Sociales",
         "description": "Simulanción de conversaciones en diferentes escenarios sociales, permitiendo al usuario practicar cómo responder de manera adecuada.",
+        "active": true,
         "categoryActivitiesId": 3,
         "disorderId": 3
     },
     {
         "title": "Coordinación Motora y Sensorial",
         "description": "Actividad en la que el usuario debe arrastrar objetos a su lugar correcto, mejorando la coordinación mano-ojo.",
+        "active": false,
         "categoryActivitiesId": 4,
         "disorderId": 3
     },
     {
         "title": "Resolución de Problemas y Lógica",
         "description": "Actividad que desafía al usuario a resolver problemas visuales o lógicos, fomentando habilidades de pensamiento crítico y resolución de problemas.",
+        "active": false,
         "categoryActivitiesId": 5,
         "disorderId": 3
     },
     {
         "title": "Relajación y Mindfulness",
         "description": "Actividad que enseña a al usuario a controlar su respiración para relajarse.",
+        "active": false,
         "categoryActivitiesId": 6,
         "disorderId": 3
     }
