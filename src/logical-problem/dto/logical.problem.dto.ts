7baab68ab66d9ee247d46e7ef3690149c98bcee9
Rodriasd <veronrodrigo98@gmail.com>
2024-11-19 11:57:16 -0300

    added model for the setting of the activity of logical problem

--- src/logical-problem/dto/logical.problem.dto.ts
+++ src/logical-problem/dto/logical.problem.dto.ts
@@ -3,4 +3,10 @@ export class createLogicalProblemSettingDto {
 
     @IsNotEmpty({message:'No se seleccionó ninguna complejidad.'})
     complexity: string;
+
+    @IsNotEmpty({message:'No se seleccionó ninguna complejidad tema.'})
+    theme: string;
+
+    @IsNotEmpty({message:'No se seleccionó ningun tipo de problema.'})
+    problem_type: string;
 };
--- src/logical-problem/logical.problem.service.ts
+++ src/logical-problem/logical.problem.service.ts
@@ -1,12 +1,14 @@
 import { PrismaService } from "src/prisma/prisma.service";
 import { Injectable } from "@nestjs/common";
-
+import { createLogicalProblemSettingDto } from "./dto/logical.problem.dto";
 
 @Injectable()
 export class LogicalProblemService {
     constructor(private readonly prismaService: PrismaService){}
 
-    async addSetting (){
-        
-    }
+    async addSetting (professionalId: number,patientId:number,setting:createLogicalProblemSettingDto){
+        return await this.prismaService.logicalProblemSetting.create({
+            data: {...setting, professionalId: professionalId, patientId: patientId}
+        });
+    };  
 };
