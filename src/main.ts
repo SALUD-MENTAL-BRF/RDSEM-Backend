7e7ecd6c9bb01105bd8e705cfd5fc145cff6532e
Rodriasd <veronrodrigo98@gmail.com>
2024-11-29 11:19:04 -0300

    fixed model of the history of the activities

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
-    origin: 'http://localhost:4000',
+    origin: ['http://localhost:4000',
+            'http://192.168.81.53:3000'
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
